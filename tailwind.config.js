9315df5dd1d49b77c69d0bd4a36cd86ed0662704
jacsatblutui <jacs@blutui.com>
2023-09-25 10:42:59 +1300

    set up project

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+  "./views/**/*.html",
+],
+theme: {
+  extend: {
+  },
+},
+plugins: [
+      require('@tailwindcss/typography'),
+]
+}
+
+
